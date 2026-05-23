8b318044a5d5131b10cf9433821c7e4ba732ef51
Pere Roca <peredanrg03@gmail.com>
2026-05-23 11:59:06 +0200

    Quarkus doesnt like his own autogenerated tests

--- src/native-test/java/com/chaintransfer/IndexResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.chaintransfer;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class IndexResourceIT extends IndexResourceTest {
-    // Execute the same tests but in packaged mode.
-}
